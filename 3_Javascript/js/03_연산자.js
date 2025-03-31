a93923332dae07e8565c52dd604947fcf1028943
miae094 <miae094@gmail.com>
2025-03-31 10:04:54 +0900

    JS_반복문 분기문

--- 3_Javascript/js/03_연산자.js
+++ 3_Javascript/js/03_연산자.js
@@ -38,3 +38,45 @@ function plusFn(){
     result.innerText = Number(value1) + Number(value2);
 }
 
+// 빼기 함수
+function minusFn() {
+    const value1 = Number(number1.value);
+    const value2 = Number(number2.value);
+  
+    result.innerText = value1 - value2;
+  }
+  // 곱하기 함수
+  function multiFn() {
+    const value1 = Number(number1.value);
+    const value2 = Number(number2.value);
+  
+    result.innerText = value1 * value2;
+  }
+  // 나누기 함수
+  function divFn() {
+    const value1 = Number(number1.value);
+    const value2 = Number(number2.value);
+  
+    result.innerText = value1 / value2;
+  }
+  // 나머지 구하는 함수
+  function modFn() {
+    const value1 = Number(number1.value);
+    const value2 = Number(number2.value);
+  
+    result.innerText = value1 % value2;
+  }
+  // -----------------------------------------
+  
+  const num1 = document.getElementById("num1");
+  const num2 = document.getElementById("num2");
+  const num3 = document.getElementById("num3");
+  const total = document.getElementById("total");
+  
+  function totalFn() {
+    const value1 = Number(num1.value);
+    const value2 = Number(num2.value);
+    const value3 = Number(num3.value);
+  
+    total.innerText = value1 + value2 + value3;
+  }
