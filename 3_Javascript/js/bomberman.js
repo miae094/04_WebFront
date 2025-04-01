5c2e42c5381808f5059b03cbbc3f25b07393ec9f
miae094 <miae094@gmail.com>
2025-04-02 01:38:16 +0900

    bomberman

--- 3_Javascript/js/bomberman.js
+++ 3_Javascript/js/bomberman.js
@@ -3,13 +3,27 @@ const box = document.getElementById("box");
 
 document.addEventListener("keydown", function(e){
     const bomberman = document.getElementById("bomberman");
-    const bomb = document.querySelector("#bomb");
-    const boomm = document.querySelector("#boomm");
     // 위 코드를 전역변수로 설정시 innerHTML 을 실행하는 과정에서 DOM 이 리렌더링 됨.
     // 기존에 const bomberman = document.getElementById("bomberman"); 이 
     // 유효하지않음
     //ArrowUp //ArrowDown //ArrowRight //ArrowLeft
-  if(e.key == 'ArrowUp'){
+    let x = bomberman.offsetLeft;
+    let y = bomberman.offsetTop;
+
+    // 이동 (transform 대신 left/top 직접 수정)
+    if (e.key === "ArrowUp") {
+        bomberman.style.top = `${y - 10}px`;
+    }
+    if (e.key === "ArrowDown") {
+        bomberman.style.top = `${y + 10}px`;
+    }
+    if (e.key === "ArrowRight") {
+        bomberman.style.left = `${x + 10}px`;
+    }
+    if (e.key === "ArrowLeft") {
+        bomberman.style.left = `${x - 10}px`;
+    }
+/*   if(e.key == 'ArrowUp'){
     console.log(e.key);
     bomberman.style.transform += `translateY(-10px)`;
   }
@@ -24,14 +38,16 @@ document.addEventListener("keydown", function(e){
   if(e.key == 'ArrowLeft'){
     console.log(e.key);
     bomberman.style.transform += `translateX(-10px)`;
-  }
+  } */
 
 
   // 폭탄투하
   let currKey = e.key.toLowerCase();
   if (currKey == "x"){
     
-    box.innerHTML += `<img src = "../../images/bomb.png" id="bomb" positon: absoulte>`;
+    //box.innerHTML += `<img src = "../../images/bomb.png" id="bomb">`;
+    //left:${x}px; top:${y}px;
+    box.innerHTML += `<img src="../../images/bomb.png" class="bomb" style="position:absolute;left:${x}px; top:${y}px;">`;
     
   }
 
@@ -39,8 +55,7 @@ document.addEventListener("keydown", function(e){
    if (currKey == "z"){
      console.log(e.key);
 
-     box.innerHTML += `<img src = "../../images/boomm.png" id="boomm">`;
+     //box.innerHTML += `<img src = "../../images/boomm.png" id="boomm">`;
+     box.innerHTML = box.innerHTML.replace(/bomb\.png/g, "boomm.png");
    }
 });
-
-
